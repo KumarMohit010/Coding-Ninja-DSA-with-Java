25c36a0f68f2a94e5f8c29801d418a1cec7ada47
KumarMohit010 <114435932+KumarMohit010@users.noreply.github.com>
2023-01-02 13:43:18 +0530

    Add files via upload

---
+++ Lecture 2 Coding Ninjas/Addingtwonumbers.java
@@ -0,0 +1,9 @@
+public class Addingtwonumbers {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 28;
+        int add = a + b;
+        System.out.println(add);
+    }
+    
+}
