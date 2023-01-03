b0aec8e6e8c2bf6312d21ab341dc923089c57378
KumarMohit010 <114435932+KumarMohit010@users.noreply.github.com>
2023-01-04 00:27:08 +0530

    Add files via upload

---
+++ Lecture 2 Coding Ninjas/Simpleinterestcalculator.java
@@ -0,0 +1,10 @@
+public class Simpleinterestcalculator {
+    public static void main(String[] args) {
+        int p = 500;
+        int r = 10;
+        int t = 5;
+        int si = (p*r*t)/100;
+        System.out.println(si);
+
+    }
+}
