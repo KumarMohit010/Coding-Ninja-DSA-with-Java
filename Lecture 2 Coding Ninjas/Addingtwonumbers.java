361b97301b35a545f4e8abab5690668d94c12907
KumarMohit010 <114435932+KumarMohit010@users.noreply.github.com>
2023-01-07 01:53:02 +0530

    Update Addingtwonumbers.java

--- Lecture 2 Coding Ninjas/Addingtwonumbers.java
+++ Lecture 2 Coding Ninjas/Addingtwonumbers.java
@@ -1,8 +1,8 @@
 public class Addingtwonumbers {
     public static void main(String[] args) {
         int a = 12;
-        int b = 2;
-        int add = a + b;
+        int l = 2;
+        int add = a + l;
         System.out.println(add);
     }
     
