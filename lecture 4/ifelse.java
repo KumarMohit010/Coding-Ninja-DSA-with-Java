e4e8039442fa5becaf230211a973bf9c132e7a9a
KumarMohit010 <114435932+KumarMohit010@users.noreply.github.com>
2023-01-09 01:52:02 +0530

    Add files via upload

---
+++ lecture 4/ifelse.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * ifelse
+ */
+public class ifelse {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int m = s.nextInt();
+        if (m % 2 == 0) {
+            System.out.println( "number is even");
+        } else {
+            System.out.println( "number is odd");
+        }
+    }
+}
