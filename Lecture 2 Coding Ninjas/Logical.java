1630a0f2e48158d989efd61fca3d50c151aef3ca
KumarMohit010 <114435932+KumarMohit010@users.noreply.github.com>
2023-01-17 22:44:10 +0530

    Update Logical.java

--- Lecture 2 Coding Ninjas/Logical.java
+++ Lecture 2 Coding Ninjas/Logical.java
@@ -5,7 +5,7 @@ public class Logical {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		int a = s.nextInt();
-		int b = s.nextInt();
+		int j = s.nextInt();
 		int c = s.nextInt();
 		
 		boolean isALargest = (a >= b) && (a >= c);
