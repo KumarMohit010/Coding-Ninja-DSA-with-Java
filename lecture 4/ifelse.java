0530c7af782f0138d78fc962686f828b7c68b7c2
KumarMohit010 <114435932+KumarMohit010@users.noreply.github.com>
2023-01-21 23:51:55 +0530

    Update ifelse.java

--- lecture 4/ifelse.java
+++ lecture 4/ifelse.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
  */
 public class ifelse {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         Scanner s = new Scanner(System.in);
         int m = s.nextInt();
         if (m % 2 == 0) {
