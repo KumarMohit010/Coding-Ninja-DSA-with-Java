4ece40e762a4f145d0f819fd458571ee6303c8a7
KumarMohit010 <114435932+KumarMohit010@users.noreply.github.com>
2023-01-22 16:29:08 +0530

    Update HelloWorld.java

--- Lecture 2 Coding Ninjas/HelloWorld.java
+++ Lecture 2 Coding Ninjas/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello Wo rld");// When you enter ln in last of print it adds a new line
+        System.out.println("Hello Wo rl d");// When you enter ln in last of print it adds a new line
         System.out.print("Hello World"); 
        
     }
