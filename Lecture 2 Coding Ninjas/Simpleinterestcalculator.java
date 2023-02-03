44aad85982d6907cd0965e9b16db2c01faac3012
KumarMohit010 <114435932+KumarMohit010@users.noreply.github.com>
2023-02-03 23:41:50 +0530

    Update Simpleinterestcalculator.java

--- Lecture 2 Coding Ninjas/Simpleinterestcalculator.java
+++ Lecture 2 Coding Ninjas/Simpleinterestcalculator.java
@@ -1,6 +1,6 @@
 public class Simpleinterestcalculator {
     public static void main(String[] args) {
-        int p = 50;
+        int p = 70;
         int r = 10;
         int t = 5;
         int si = (p*r*t)/100;
