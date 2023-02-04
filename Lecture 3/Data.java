4e8d5a6726d757dd99b5000ff824a99678778bec
KumarMohit010 <114435932+KumarMohit010@users.noreply.github.com>
2023-02-04 21:45:41 +0530

    Update Data.java

--- Lecture 3/Data.java
+++ Lecture 3/Data.java
@@ -5,7 +5,7 @@ public class Data{
         char c ='a';
         // System.out.println(c+3);
         //here it will print hundred because it is printing the value of a in ascii
-        int i = c + 10;
+        int a = c + 10;
         System.out.println(i);
         //you can copy int to long but you can't copy bigger data type into smaller ones
         //as you can copy integer in double but you can't copy double into integer
@@ -16,7 +16,7 @@ public class Data{
 float f =1.23f; //you have to declare 1.23 as f float so that it wont be stores as double because by default every floating point number is stored as double in memory
 // d=f; valid;
 // f=d not valid;
-for copying long data into smaller one we use explicit typecasting as i = (int)l;
+for copying long data into smaller one we use explicit typecasting as a = (int)l;
  
 
     }
