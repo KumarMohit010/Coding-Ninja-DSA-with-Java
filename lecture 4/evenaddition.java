3e0cdf14a95950b3d74f401347b9b2ec7a616353
KumarMohit010 <114435932+KumarMohit010@users.noreply.github.com>
2023-02-10 22:04:11 +0530

    Update evenaddition.java

--- lecture 4/evenaddition.java
+++ lecture 4/evenaddition.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
     public class evenaddition {
     
         public static void main(String[] args) {
-            int m;
+            int n;
             Scanner sc = new Scanner(System.in);
             m=sc.nextInt();
     
             int i=2, sum=0;
             
-            while(i <= m) {
+            while(i <= n ) {
                 sum +=  i;
                 i = i+2;
             }
