fce42a17defdf6a9f09fcb87869bf72d48b0ed50
KumarMohit010 <114435932+KumarMohit010@users.noreply.github.com>
2023-02-11 23:54:21 +0530

    Update ifelse.java

--- lecture 4/ifelse.java
+++ lecture 4/ifelse.java
@@ -9,7 +9,7 @@ public class ifelse {
         Scanner s = new Scanner(System.in);
         int m = s.nextInt();
         if (m % 2 == 0) {
-            System.out.println( "number is even");
+            System.out.println( "number Is even");
         } else {
             System.out.println( "number is odd");
         }
